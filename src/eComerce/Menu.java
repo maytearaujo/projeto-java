e32dda134b19335fc7d870a241239fe08cca9fba
Maytê <maytearaujo@yahoo.com.br>
2025-02-20 11:52:20 -0300

    feat: Desenvolvimento do Menu

--- src/eComerce/Menu.java
+++ src/eComerce/Menu.java
@@ -1,11 +1,83 @@
 package eComerce;
 
+import java.util.Scanner;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// E-commerce Produtos Esportivos
-		System.out.println("Hello World!!!");
 
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		while (true) {
+
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("                E-commerce Sports Brasil             ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Produto                        ");
+			System.out.println("            2 - Listar todos os Produtos             ");
+			System.out.println("            3 - Buscar Produto por Numero            ");
+			System.out.println("            4 - Atualizar Dados do Produto           ");
+			System.out.println("            5 - Apagar Produto                       ");
+			System.out.println("            6 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     ");
+
+			opcao = leia.nextInt();
+			
+			if (opcao == 6) {
+				System.out.println("\nAgradecemos por utilizar o nosso programa !");
+				sobre();
+                leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case 1:
+					System.out.println("Criar Produto\n\n");
+
+					break;
+				
+				case 2:
+					System.out.println("Listar todos os Produtos\n\n");
+
+					break;
+				
+				case 3:
+					System.out.println("Consultar Produto - código de barra\n\n");
+
+					break;
+				
+				case 4:
+					System.out.println("Atualizar Produto\n\n");
+
+					break;
+				
+				case 5:
+					System.out.println("Apagar Produto\n\n");
+
+					break;
+				
+				default:
+					System.out.println("\nOpção Inválida!\n");
+					break;
+			}
+		}
 	}
+    		
+		public static void sobre() {
+			System.out.println("\n*********************************************************");
+			System.out.println("Projeto Desenvolvido por: ");
+			System.out.println("Maytê Araujo - maytearaujo@yahoo.com.br");
+			System.out.println("https://github.com/maytearaujo");
+			System.out.println("*********************************************************");
+		}
 
 }
